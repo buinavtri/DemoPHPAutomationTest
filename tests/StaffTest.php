2dfc435d0d9985a94a3e2227c12b41e5dda92622
Bui Van Tri <buinavtri@gmail.com>
2020-09-23 14:21:04 +0700

    Fix tri 1

--- tests/StaffTest.php
+++ tests/StaffTest.php
@@ -27,7 +27,7 @@ class StaffTest extends TestCase
     public function only_first_name()
     {
         $staff = new Staff;
-        $staff->first_name = "Tri";
-        $this->assertEquals('Tri', $staff->getFullName());
+        $staff->first_name = "Tri 1";
+        $this->assertEquals('Tri 1', $staff->getFullName());
     }
 }
